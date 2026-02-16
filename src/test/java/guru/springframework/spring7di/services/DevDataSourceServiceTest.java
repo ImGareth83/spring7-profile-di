e5a7e7326934501ed9a57f59deecbd4ca48566ef
Gareth Fong <garethfong@icloud.com>
2026-02-16 11:09:13 +0800

    Clean up data source service tests

--- src/test/java/guru/springframework/spring7di/services/DevDataSourceServiceTest.java
+++ src/test/java/guru/springframework/spring7di/services/DevDataSourceServiceTest.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import guru.springframework.spring7di.services.DataSourceService;
-
 @ActiveProfiles("dev")
 @SpringBootTest
 public class DevDataSourceServiceTest {
